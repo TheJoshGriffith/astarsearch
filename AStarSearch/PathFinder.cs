011cb91855588b73edb89814e68ff564e4d2ba12
XtrmJosh@gmail.com <XtrmJosh@gmail.com@0ba10f5b-06de-62e2-581a-155a9ebae2b9>
2013-02-19 21:02:25 +0000

    Uploaded startup code.

---
+++ AStarSearch/PathFinder.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AStarSearch
+{
+    class PathFinder
+    {
+        public Structs.Tile[] map;
+
+        public Structs.Tile[] Map
+        {
+            set{ map = value; }
+            get{ return map; }
+        }
+
+        public int getTile(int x, int y, Structs.Tile[] map)
+        {
+            int i = 0;
+            foreach (Structs.Tile til in map)
+            {
+                if (til.x == x && til.y == y)
+                {
+                    return i;
+                }
+                i++;
+            }
+            return 0;
+        }
+
+        public List<Structs.Tile> getSurroundingTiles(Structs.Tile baseTile)
+        {
+            List<Structs.Tile> tiles = new List<Structs.Tile>();
+            foreach (Structs.Tile tile in map)
+            {
+                if (Math.Abs(tile.x - baseTile.x) <= 1 && Math.Abs(tile.y - baseTile.y) <= 1)
+                {
+                   tiles.Add(tile);
+                }
+            }
+            return tiles;
+        }
+
+        public List<Structs.Tile> findPath(int startx, int starty, int goalx, int goaly, Structs.Tile[] map)
+        {
+            List<Structs.Tile> closedList = new List<Structs.Tile>();
+            List<Structs.Tile> openList = new List<Structs.Tile>();
+            List<Structs.Tile> pathList = new List<Structs.Tile>();
+
+            Structs.Tile Current = new Structs.Tile();
+            int i = 0; // This is for the if loop
+            int index = 0; // This is for storing which tile we are currently stood on.
+            Current.x = startx;
+            Current.y = starty;
+            foreach (Structs.Tile tile in map)
+            {
+                if (tile.x == Current.x && tile.y == Current.y)
+                {
+                    index = i;
+                }
+                i++;
+            }
+
+            foreach (Structs.Tile tile in map)
+            {
+                foreach (Structs.Tile tile1 in getSurroundingTiles(Current))
+                {
+                    if (tile1.x == tile.x && tile1.y == tile.y)
+                    {
+                        Structs.Tile curTile = tile1;
+                        curTile.parentid = Current.id;
+                        curTile.distanceLeft = (Math.Abs(tile.x - goalx) + Math.Abs(tile.y - goaly));
+                        openList.Add(curTile);
+                    }
+                }
+            }
+            return pathList;
+        }
+    }
+}
---
+++ AStarSearch/Structs.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AStarSearch
+{
+    class Structs
+    {
+        public struct Tile
+        {
+            public int id;
+            public int x;
+            public int y;
+            public int cost;
+            public bool walkable;
+            public int distanceLeft;
+            public int parentid;
+        }
+    }
+}
